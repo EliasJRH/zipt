17958d09ec9c383441b37a53f967fe6720e47ea6
Elias Hawa <elias.hawa2468@gmail.com>
2022-10-26 11:27:04 -0400

    add help text

--- index.mjs
+++ index.mjs
@@ -7,106 +7,138 @@ import CheckboxPlusPrompt from "inquirer-checkbox-plus-prompt";
 import chalk from "chalk";
 import fs from "fs";
 
-let dirPath = process.cwd();
-let choices = fs.readdirSync(dirPath);
+const help = function () {
+  const helpText = `tzip
 
-const zipFormats = ["zip", "tar"]
+tzip is an interactive command line application for creating compressed archives
 
-inquirer.registerPrompt("checkbox-plus", CheckboxPlusPrompt);
+usage:
+  tzip <command>
 
-inquirer
-  .prompt([
-    {
-      type: "input",
-      name: "zipName",
-      message: "Enter name of zip file:",
-    },
-    {
-      type: "checkbox-plus",
-      name: "filesToZip",
-      message: "Select all files to be added to zip folder. Select " + chalk.bold(chalk.cyan("<Space>")) + " to add file and " + chalk.bold(chalk.cyan("type")) + " to search for files",
-      pageSize: 10,
-      highlight: true,
-      searchable: true,
-      source: function (answersSoFar, input) {
-        input = input || "";
+  -h, --help: Diplays this help message
+  -r: Display individual files in nested subdirectories
+  `;
 
-        return new Promise(function (resolve) {
-          var fuzzyResult = fuzzy.filter(input, choices);
+  console.log(helpText);
+};
 
-          var data = fuzzyResult.map(function (element) {
-            return element.original;
-          });
+const main = function () {
+  console.log(getNestedFiles())
 
-          resolve(data);
-        });
+  let dirPath = process.cwd();
+  let files = fs.readdirSync(dirPath);
+
+  const zipFormats = ["zip", "tar"];
+
+  inquirer.registerPrompt("checkbox-plus", CheckboxPlusPrompt);
+
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "zipName",
+        message: "Enter name of zip file:",
       },
-    },
-    {
-      type: "list",
-      name: "zipFormat",
-      message: "Select compression file type",
-      choices: zipFormats
-    }
-  ])
-  .then((answers) => {
-    createZip(answers.zipName, answers.filesToZip, answers.zipFormat);
-  });
+      {
+        type: "checkbox-plus",
+        name: "filesToZip",
+        message:
+          "Select all files to be added to zip folder. Select " +
+          chalk.bold(chalk.cyan("<Space>")) +
+          " to add file and " +
+          chalk.bold(chalk.cyan("type")) +
+          " to search for files",
+        pageSize: 10,
+        highlight: true,
+        searchable: true,
+        source: function (answersSoFar, input) {
+          input = input || "";
 
-function createZip(zipName, items, zipFormat) {
-  console.log(zipFormat)
-  // The following few lines of code is boilerplate archiver code
+          return new Promise(function (resolve) {
+            var fuzzyResult = fuzzy.filter(input, files);
 
-  // create a file to stream archive data to.
-  const output = fs.createWriteStream(`${dirPath}/${zipName}.${zipFormat}`);
-  const archive = archiver(zipFormat, {
-    zlib: { level: 9 }, // Sets the compression level.
-  });
+            var data = fuzzyResult.map(function (element) {
+              return element.original;
+            });
 
-  // listen for all archive data to be written
-  // 'close' event is fired only when a file descriptor is involved
-  output.on("close", function () {
-    console.log(archive.pointer() + " total bytes");
-    console.log(
-      "archiver has been finalized and the output file descriptor has closed."
-    );
-  });
+            resolve(data);
+          });
+        },
+      },
+      {
+        type: "list",
+        name: "zipFormat",
+        message: "Select compression file type",
+        choices: zipFormats,
+      },
+    ])
+    .then((answers) => {
+      createZip(answers.zipName, answers.filesToZip, answers.zipFormat);
+    });
 
-  // This event is fired when the data source is drained no matter what was the data source.
-  // It is not part of this library but rather from the NodeJS Stream API.
-  // @see: https://nodejs.org/api/stream.html#stream_event_end
-  output.on("end", function () {
-    console.log("Data has been drained");
-  });
+  function createZip(zipName, items, zipFormat) {
+    console.log(zipFormat);
+    // The following few lines of code is boilerplate archiver code
 
-  // good practice to catch warnings (ie stat failures and other non-blocking errors)
-  archive.on("warning", function (err) {
-    if (err.code === "ENOENT") {
-      // log warning
-    } else {
-      // throw error
+    // create a file to stream archive data to.
+    const output = fs.createWriteStream(`${dirPath}/${zipName}.${zipFormat}`);
+    const archive = archiver(zipFormat, {
+      zlib: { level: 9 }, // Sets the compression level.
+    });
+
+    // listen for all archive data to be written
+    // 'close' event is fired only when a file descriptor is involved
+    output.on("close", function () {
+      console.log(archive.pointer() + " total bytes");
+      console.log(
+        "archiver has been finalized and the output file descriptor has closed."
+      );
+    });
+
+    // This event is fired when the data source is drained no matter what was the data source.
+    // It is not part of this library but rather from the NodeJS Stream API.
+    // @see: https://nodejs.org/api/stream.html#stream_event_end
+    output.on("end", function () {
+      console.log("Data has been drained");
+    });
+
+    // good practice to catch warnings (ie stat failures and other non-blocking errors)
+    archive.on("warning", function (err) {
+      if (err.code === "ENOENT") {
+        // log warning
+      } else {
+        // throw error
+        throw err;
+      }
+    });
+
+    // good practice to catch this error explicitly
+    archive.on("error", function (err) {
       throw err;
-    }
-  });
+    });
 
-  // good practice to catch this error explicitly
-  archive.on("error", function (err) {
-    throw err;
-  });
+    // pipe archive data to the file
+    archive.pipe(output);
 
-  // pipe archive data to the file
-  archive.pipe(output);
+    items.forEach((item) => {
+      let statSync = fs.lstatSync(item);
+      if (statSync.isFile()) {
+        archive.file(item, { name: item });
+      } else if (statSync.isDirectory()) {
+        archive.directory(`${item}/`, item);
+      }
+    });
 
-  items.forEach((item) => {
-    let statSync = fs.lstatSync(item);
-    if (statSync.isFile()) {
-      archive.file(item, { name: item });
-    }else if (statSync.isDirectory()){
-      archive.directory(`${item}/`, item);
-    }
-  });  
+    // finalize the archive (ie we are done appending files but streams have to finish yet)
+    // 'close', 'end' or 'finish' may be fired right after calling this method so register to them beforehand
+    archive.finalize();
+  }
+};
 
-  // finalize the archive (ie we are done appending files but streams have to finish yet)
-  // 'close', 'end' or 'finish' may be fired right after calling this method so register to them beforehand
-  archive.finalize();
+const args = process.argv;
+
+if (process.argv.includes("-h") || process.argv.includes("--help")) {
+  help();
+} else {
+  main();
 }
